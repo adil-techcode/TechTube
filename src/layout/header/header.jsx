8921e7b7b2d07990cc7f9ddf9415a07829ba8b8a
adil <adil@gmail.com>
2023-11-17 02:31:03 +0500

    beta done

---
+++ src/layout/header/header.jsx
@@ -0,0 +1,103 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from "react-bootstrap/Dropdown";
+import logo from "../../assests/logo.png";
+import styles from "./header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = ({ fixed }) => {
+  var user;
+  try {
+    user = JSON.parse(localStorage.getItem("user"));
+  } catch (error) {
+    console.log(error);
+  }
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" fixed={fixed ? "top" : ""}>
+      <Container fluid>
+        <Navbar.Brand>
+          <Link to={"/"}>
+            <img src={logo} alt="" width={200} height={50} />{" "}
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0">
+            {user && (
+              <Nav.Link>
+                {" "}
+                <Link
+                  to={"/courses"}
+                  className="  fs-5 text-black text-decoration-none"
+                >
+                  {" "}
+                  Courses{" "}
+                </Link>{" "}
+              </Nav.Link>
+            )}
+            <Nav.Link>
+              {" "}
+              <Link
+                to={"/about"}
+                className="  fs-5 text-black text-decoration-none"
+              >
+                {" "}
+                About{" "}
+              </Link>{" "}
+            </Nav.Link>
+            <Nav.Link>
+              {" "}
+              <Link
+                to={"/contact"}
+                className="  fs-5 text-black text-decoration-none"
+              >
+                {" "}
+                Contact{" "}
+              </Link>{" "}
+            </Nav.Link>
+          </Nav>
+          {user ? (
+            <div className="d-flex">
+              <img
+                src={user.photo}
+                alt="User Avatar"
+                className="rounded-circle mr-3 "
+                style={{ width: 40, height: 40, objectFit: "cover" }}
+              />
+              <Dropdown className={`mr-3   ${styles.dropdownBox}  `}>
+                <Dropdown.Toggle
+                  variant="success"
+                  id="dropdown-basic"
+                  className={`mr-3 ${styles.dropdownBody} `}
+                >
+                  {user.name.toUpperCase()}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1"> Logout </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          ): (
+
+          <div className="d-flex">
+            <Link to={"signin"}  >    <button type="button" className= {`btn btn-success ${styles.signinBtn}` }   >
+              Sign in
+            </button> </Link>  
+            <Link to={"signup"} > 
+            <button type="button" className= {`btn mx-2 btn-outline-success ${styles.signupBtn}` }   >
+              Sign up
+            </button> </Link>
+          </div>)}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/router/routes.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { createBrowserRouter, Route, Navigate } from "react-router-dom";
+import SignIn from "../pages/signin/signIn.jsx";
+import Signup from "../pages/signup/signUp.jsx";
+import About from "../pages/about/about.jsx";
+import Contact from "../pages/contact/contact.jsx";
+import Courses from "../pages/courses/courses.jsx";
+import Admin from "../pages/admin/admin.jsx";
+import Ytvideo from "../pages/playlistvideos/ytvideo.jsx";
+import Home from "../pages/landingpage/home.jsx";
+import { AES } from "crypto-js";
+import CryptoJS from "crypto-js";
+import ForbiddenPage from "../errorpages/403.jsx";
+import NotFoundPage from "../errorpages/404.jsx";
+
+
+
+const ProtectedRoute = ({ element: Element }) => {
+    try{
+        const user = JSON.parse(localStorage.getItem("user")); // Parse the user data from localStorage
+        return user ? <Element /> : <Navigate to="/signin" />;
+    }
+    catch(error){
+        return <Navigate to="/signin" />;
+    }
+
+};
+
+const AdminRoute = ({ element: Element }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user) {
+    try {
+      const pass = AES.decrypt(user.password, "adilhon").toString(
+        CryptoJS.enc.Utf8
+      );
+      return pass == "adil520" ? <Element /> : <Navigate to="/signin" />;
+    } catch (error) {
+      return <Navigate to="/forbiddenpage" />;
+    }
+  }
+  return <Navigate to="/signin" />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/courses",
+    element: <ProtectedRoute element={Courses} />,
+  },
+  {
+    path: "/admin",
+    element: <AdminRoute element={Admin} />,
+  },
+  {
+    path: "/playlist/:id",
+    element: <ProtectedRoute element={Ytvideo} />,
+  },
+  {
+    path: "/forbiddenpage",
+    element: <ForbiddenPage/>,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage/>,
+  },
+]);
+
+export default router;
